244881f9f464b37db1deb174b816aa9b96a5686c
Beverly Pham <beverly.pham@gmail.com>
2018-01-22 13:36:33 -0800

    pseudocode base plan

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// array to store all products
+
+// constructor for Product objects
+// properties: name of object, filepath, number of times shown, number of times clicked, HTML id string
+// property: 
+
+// create instances
+
+// access image elements from DOM
+
+// add event listener on the images
+
+// callback function for the event listener to display 3 more items
+// track number times image is displayed 
+// track number of clicks on image
+
+// invoke callback on page load to display first 3 images
+// make sure images were not immediately previously displayed
+
+// after 25 selections, turn off event listener and display products and votes received -- display on new page? display item or just name with results?
