3929d29b4929ef386a6a42004cc418ee45426dd7
Beverly Pham <beverly.pham@gmail.com>
2018-01-22 14:09:58 -0800

    create instances and edit image files

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
 'use strict';
 
 // array to store all products
+Product.allProducts = [];
 
 // constructor for Product objects
 // properties: name of object, filepath, number of times shown, number of times clicked, HTML id string
-// property: 
+function Product(productName, filepath, htmlId) {
+  this.productName = productName;
+  this.filepath = filepath;
+  this.htmlId = htmlId;
+  this.timesProductShown = 0;
+  this.productSelectedTally = 0;
+  Product.allProducts.push(this);
+}
 
 // create instances
+new Product('Star Wars Bag', 'img/bag.jpg', 'star-wars-bag');
+new Product('Banana Slicer', 'img/banana.jpg', 'banana-slicer');
+new Product('Bathroom Helper', 'img/bathroom.jpg', 'bathroom-helper');
+new Product('Open-Toed Boots', 'img/boots.jpg', 'boots');
+new Product('All-In-One Breakfast Oven', 'img/breakfast.jpg', 'breakfast-oven');
+new Product('Meatball Bubble Gum', 'img/bubblegum.jpg', 'bubble-gum');
+new Product('Red Funky Chair', 'img/chair.jpg', 'chair');
+new Product('Cthulhu Figurine', 'img/cthulhu.jpg', 'cthulhu');
+new Product('Dog Duck', 'img/dog-duck.jpg', 'dog-duck');
+new Product('Dragon Meat', 'img/dragon.jpg', 'dragon-meat');
+new Product('Utensil Pen Topper', 'img/pen.jpg', 'pen-topper');
+new Product('Pet Sweep', 'img/pet-sweep.jpg', 'pet-sweep');
+new Product('Pizza Scissors', 'img/scissors.jpg', 'pizza-scissors');
+new Product('Shark Sleeping Bag', 'img/shark.jpg', 'shark');
+new Product('Onesie Sweep', 'img/sweep.png', 'onesie-sweep');
+new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg', 'tauntaun');
+new Product('Unicorn Meat', 'img/unicorn.jpg', 'unicorn-meat');
+new Product('Tentacle USB', 'img/usb.gif', 'tentacle-usb');
+new Product('Self Water Can', 'img/water-can.jpg', 'water-can');
+new Product('Wine Glass', 'img/wine-glass.jpg', 'wine-glass');
 
 // access image elements from DOM
 
